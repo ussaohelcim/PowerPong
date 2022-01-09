10eb2410025362f57c1ecd9468e9750c55d1165e
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-01-09 18:28:10 -0300

    adicionado sons

--- pong.ps1
+++ pong.ps1
@@ -12,6 +12,12 @@ $raqueteDireita = [engine.Rectangle2D]::GetNew(380,150,20,50,0,254,0,254)
 
 $raqueteEsquerda = [engine.Rectangle2D]::GetNew(0,150, 20,50,0,0,254,254)
 
+$engine.EnableAudioDevice()
+
+[Raylib_cs.Sound]$somRaquete = $engine.LoadSoundFromFile("assets\sounds\sfxRaquete.wav")
+[Raylib_cs.Sound]$somTeto = $engine.LoadSoundFromFile("assets\sounds\sfxTetoCHao.wav")
+#$somRaquete = $engine.LoadSoundFromFile(".\sfxRaquete.wav")
+
 $w = 400
 $h = 300
 
@@ -27,11 +33,13 @@ $pontos = @{
 $velBola = @{
 	x = 0
 	y = 0
+	multiplicador = 1
 }
 function ZerarBola { param($vel)
 
 	$quadrado.PosX = $w/2
 	$quadrado.PosY = $h/2
+	$velBola.multiplicador = 1
 
 	$vel.x = ((-1,1) | Get-Random)*5
 	$vel.y = 0
@@ -40,30 +48,65 @@ function PrintarFirulas {param ($txt, [engine.Rectangle2D]$retangulo)
 	Write-Host $txt ' x ' $retangulo.PosX ' y ' $retangulo.PosY ' w ' $retangulo.largura ' h ' $retangulo.altura
 }
 
+function CrescerQuadrado { param ( $tam)
+	$largura = $quadrado.largura
+	$altura = $quadrado.altura
+	$aMax = $retangulo.altura * $tam
+	$lMax = $quadrado.largura * $tam
+	while ($quadrado.largura -le $lMax -and $retangulo.altura -le $aMax) {
+		Start-Sleep -Milliseconds 10
+		$quadrado.largura++
+		$quadrado.altura++
+	}
+	$quadrado.altura = $altura
+	$quadrado.largura = $largura
+}
+
 ZerarBola -vel $velBola 
 
 while(!$engine.IsAskingToCloseWindow())
 {
+	
+
+
 	$quadrado.Move($velBola.x,$velBola.y)
 
 	$txtPontos = [string]$pontos.esquerda + ' x ' + [string]$pontos.direita
 
 	if($quadrado.IsCollidingWithRectangle2D($raqueteDireita))
 	{
-		$velBola.x = -5
-		$velBola.y = if($quadrado.PosY + $quadrado.altura /2 -ge $raqueteDireita.PosY + $raqueteDireita.altura /2) {5} else {-5}
+		$velBola.x = -(5 + ($velBola.multiplicador * 0.1))
+		if($velBola.y -eq 0){
+			$velBola.y = ((-1,1)|Get-Random) * 5
+		}
+		$velBola.multiplicador++
+		$engine.PlayThisSound($somRaquete)
 	}
 	elseif($quadrado.IsCollidingWithRectangle2D($raqueteEsquerda))
 	{
-		$velBola.x = 5
-		$velBola.y = if($quadrado.PosY + $quadrado.altura /2 -ge $raqueteEsquerda.PosY + $raqueteEsquerda.altura /2) {5} else {-5}
+		$velBola.x = 5 + ($velBola.multiplicador * 0.1)
+		if($velBola.y -eq 0){
+			$velBola.y = ((-1,1)|Get-Random) * 5
+		}
+		$velBola.multiplicador++
+		$engine.PlayThisSound($somRaquete)
 
+		#Write-Host $velBola.multiplicador
+		
 	}elseif($quadrado.IsCollidingWithRectangle2D($teto))
 	{
 		$velBola.y = -$velBola.y 
+		
+		$velBola.multiplicador++
+		$engine.PlayThisSound($somTeto)
+		#Write-Host $velBola.multiplicador
+		
 	}elseif($quadrado.IsCollidingWithRectangle2D($chao))
 	{
 		$velBola.y = -$velBola.y 
+		$velBola.multiplicador++
+		$engine.PlayThisSound($somTeto)
+		#Write-Host $velBola.multiplicador
 	}
 	
 	if($engine.SegurandoCima())
@@ -75,6 +118,12 @@ while(!$engine.IsAskingToCloseWindow())
 
 	}
 	else { $raqueteDireita.Move(0,0) }
+
+	if($engine.ApertouEspaco())
+	{
+		$quadrado.largura++
+		$quadrado.altura++
+	}
 	
 
 	if($engine.SegurandoW())
@@ -113,6 +162,7 @@ while(!$engine.IsAskingToCloseWindow())
     $engine.ClearFrame();
 
 	Start-Sleep -Milliseconds 16
+	
 }
 
 $engine.CloseWindow();
